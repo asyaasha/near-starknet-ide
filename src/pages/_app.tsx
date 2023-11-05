43428f5c359975cb20822011537a2b7d38ad5735
Asiya <asya.asha@gmail.com>
2023-11-05 16:13:56 -0500

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,36 +44,6 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     initializeAnalytics();
   }, []);
 
-  useEffect(() => {
-    // Displays the Zendesk widget only if user is signed in and on the home page
-    if (!window.zE) return;
-    if (!authStore.signedIn || Boolean(componentSrc?.componentAccountId && componentSrc?.componentName)) {
-      window.zE('webWidget', 'hide');
-      return;
-    }
-    localStorage.setItem('accountId', authStore.accountId);
-    window.zE('webWidget', 'show');
-  }, [authStore.accountId, authStore.signedIn, componentSrc]);
-
-  useEffect(() => {
-    const interval = setInterval(zendeskCheck, 20);
-
-    function zendeskCheck() {
-      // once the zendesk widget comes online, style it
-      const zwFrame = document.getElementById('launcher') as HTMLIFrameElement | null;
-      const zwEmbed = zwFrame?.contentDocument?.getElementById('Embed');
-      const zwButton = zwEmbed?.querySelector('[data-testid="launcher"]');
-      if (zwButton) {
-        styleZendesk();
-        clearInterval(interval);
-      }
-    }
-
-    () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   return (
     <>
       <Head>
@@ -94,54 +64,11 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       </Script>
 
       <Script id="phosphor-icons" src="https://unpkg.com/@phosphor-icons/web@2.0.3" async />
-
-      <Script
-        src="https://static.zdassets.com/ekr/snippet.js?key=1736c8d0-1d86-4080-b622-12accfdb74ca"
-        id="ze-snippet"
-        async
-      />
-
-      <Script id="zendesk-config" strategy="afterInteractive">
-        {`
-          window.zESettings = {
-            webWidget: {
-              color: { theme: '#2b2f31' },
-              offset: {
-                horizontal: '10px',
-                vertical: '10px',
-                mobile: { horizontal: '2px', vertical: '65px', from: 'right' },
-              },
-              contactForm: {
-                attachments: true,
-                title: { '*': 'Feedback and Support' },
-                fields: [
-                  {
-                    id: 13149356989591,
-                    prefill: { '*': localStorage.getItem('accountId') },
-                  },
-                ],
-              },
-            },
-          };
-        `}
-      </Script>
-
       <Script id="bootstrap" src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" />
 
       <VmInitializer />
 
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-TR5G968"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
-
       {getLayout(<Component {...pageProps} />)}
-
-      <Toaster />
     </>
   );
 }
