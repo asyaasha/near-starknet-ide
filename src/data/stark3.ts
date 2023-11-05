9ba6a65e5c54721e2ff551cca776c99cbf08f5cd
Asiya <asya.asha@gmail.com>
2023-11-05 17:38:56 -0500

    wupdat

--- src/data/stark3.ts
+++ src/data/stark3.ts
@@ -1,44 +1,39 @@
-import { connect, disconnect } from "@argent/get-starknet"
+import { connect, disconnect } from '@argent/get-starknet';
 import { useEffect, useState } from 'react';
 import { singletonHook } from 'react-singleton-hook';
-
-
 
 const defaultStProviderContext = { disconnect, arConnect: connect, arProvider: null, account: null };
 
 export const useStProviderContext = singletonHook(defaultStProviderContext, () => {
-  const initial:any = null;
+  const initial: any = null;
   const [argent, setAgent] = useState(initial);
   const [stProvider, setStProvider] = useState(defaultStProviderContext);
 
   useEffect(() => {
     (async () => {
       if (typeof localStorage === 'undefined') return;
-            try{
-              const res = await connect()
-              setAgent(res)
-              await res?.enable({ starknetVersion: "v4" })
-
-              // set account to an account state
-              localStorage.setItem('starknetKey', JSON.stringify(res));
-          }   
-        catch(error:any){
-            console.log(error.message)
-        }
+      try {
+        // const res = await connect()
+        // setAgent(res)
+        // await res?.enable({ starknetVersion: "v4" })
+        // // set account to an account state
+        // localStorage.setItem('starknetKey', JSON.stringify(res));
+      } catch (error: any) {
+        console.log(error.message);
+      }
     })();
   }, []);
 
   useEffect(() => {
-    console.log("argent -> in update")
-    console.log(argent)
-    if (!argent) return;
-
-    setStProvider({
-      disconnect: disconnect,
-      account: argent?.account,
-      arProvider: argent?.provider,
-      arConnect: connect,
-    });
+    // console.log('argent -> in update');
+    // console.log(argent);
+    // if (!argent) return;
+    // setStProvider({
+    //   disconnect: disconnect,
+    //   account: argent?.account,
+    //   arProvider: argent?.provider,
+    //   arConnect: connect,
+    // });
   }, [argent]);
 
   return stProvider;
